c92a267686951eb24256651cfb40e04fc7c8467c
David Andrs <andrsd@gmail.com>
2020-03-21 16:12:29 -0600

    Using different design for TopicsPage

--- web/app/pages/topic/index.js
+++ web/app/pages/topic/index.js
@@ -7,38 +7,48 @@ const TopicPage = ATV.Page.create({
   name: 'topic',
   template: template,
   ready (options, resolve, reject) {
-    var obj = {}
+    let getTopic = ATV.Ajax.get(API.url.topic(options.id), {})
 
-    obj.attributes = options.attributes
+    Promise
+      .all([getTopic])
+      .then((xhrs) => {
+        var data = xhrs[0].response.data
+        var obj = {}
 
-    for (var widget of options.attributes.widgets) {
-      if (widget.type == 'carousel') {
-        obj.carousel = []
-        for (var i of widget.attributes.items) {
-          if (i.itemType == 'audio')
-            obj.carousel.push(i)
-        }
-      }
-      else if (widget.type == 'shows_list') {
-        obj.shows = []
-        for (var i of widget.attributes.entities) {
-          if (i.type == 'show')
-            obj.shows.push(i)
-        }
-      }
-      else if (widget.type == 'episodes_list') {
-        obj.episodes = []
-        for (var ep of widget.attributes.entities) {
-          if (ep.type == 'episode') {
-            var e = ep
-            e.attributes.length = API.episode_time(ep.attributes.since, ep.attributes.till)
-            obj.episodes.push(e)
+        obj.attributes = data.attributes
+
+        for (var widget of data.attributes.widgets) {
+          if (widget.type == 'carousel') {
+            obj.carousel = []
+            for (var i of widget.attributes.items) {
+              if (i.itemType == 'audio')
+                obj.carousel.push(i)
+            }
+          }
+          else if (widget.type == 'shows_list') {
+            obj.shows = []
+            for (var i of widget.attributes.entities) {
+              if (i.type == 'show')
+                obj.shows.push(i)
+            }
+          }
+          else if (widget.type == 'episodes_list') {
+            obj.episodes = []
+            for (var ep of widget.attributes.entities) {
+              if (ep.type == 'episode') {
+                var e = ep
+                e.attributes.length = API.episode_time(ep.attributes.since, ep.attributes.till)
+                obj.episodes.push(e)
+              }
+            }
           }
         }
-      }
-    }
 
-    resolve(obj)
+        resolve(obj)
+      }, (xhr) => {
+        // error
+        reject()
+      })
   }
 })
 
--- web/app/pages/topics/index.js
+++ web/app/pages/topics/index.js
@@ -1,85 +1,25 @@
 import ATV from 'atvjs'
 import template from './template.hbs'
-import episodesTpl from './episodes.hbs'
 
 import API from 'lib/rozhlas.js'
 
 const TopicsPage = ATV.Page.create({
   name: 'topics',
   template: template,
-  events: {
-    highlight: 'onHighlight'
-  },
-  episodes_loaded: {},
   ready (options, resolve, reject) {
     let getTopics = ATV.Ajax.get(API.url.topics, {})
 
     Promise
       .all([getTopics])
       .then((xhrs) => {
-        let res = xhrs[0].response
-
-        var topics = []
-        for (var s of res.data) {
-          topics.push({
-            id: s.id,
-            title: s.attributes.title
-          })
-          this.episodes_loaded[s.id] = false
-        }
-
         resolve({
-          topics: topics
+          topics: xhrs[0].response.data
         })
       }, (xhr) => {
         // error
         reject()
       })
   },
-  onHighlight(e) {
-    let element = e.target
-    let elementType = element.nodeName.toLowerCase()
-
-    if (elementType === 'listitemlockup') {
-        this.loadEpisodes(element)
-    }
-  },
-  loadEpisodes (element) {
-    var topic_id = element.getAttribute("id")
-
-    var doc = getActiveDocument()
-    // Create parser and new input element
-    var domImplementation = doc.implementation
-    var lsParser = domImplementation.createLSParser(1, null)
-    var lsInput = domImplementation.createLSInput()
-
-    let getTopicEpisodes = ATV.Ajax.get(API.url.topicEpisodes(topic_id), {})
-
-    Promise
-      .all([getTopicEpisodes])
-      .then((xhrs) => {
-        let results = xhrs[0].response
-
-        var episodes = []
-        for (var r of results.data) {
-          episodes.push(r)
-        }
-
-        //overwrite stringData for new input element if search results exist by dynamically constructing shelf template fragment
-        lsInput.stringData = episodesTpl({
-          episodes: episodes
-        })
-
-        //add the new input element to the document by providing the newly created input, the context,
-        //and the operator integer flag (1 to append as child, 2 to overwrite existing children)
-        lsParser.parseWithContext(lsInput, element.getElementsByTagName("relatedContent").item(0), 2)
-
-        this.episodes_loaded[topic_id] = true
-      }, (xhr) => {
-        // error
-        reject()
-      })
-  }
 })
 
 export default TopicsPage
